715ba71e15906d57cb40b03b14708348d6da8601
yimis-source <fersungfloo@gmail.com>
2025-09-30 13:25:59 -0500

    Refactor task manager views and templates for improved functionality and structure

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.index),
     path('about', views.about),  
-    path('tasks/<int:task>', views.task_list, ),
+    path('tasks/', views.task_list, ),
     path('detail/<int:task>', views.task_detail, ),
+    path('create-task/', views.create_task,),
 
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,20 +1,27 @@
-from django.shortcuts import render,HttpResponse,get_object_or_404
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.http import JsonResponse
 from .models import Task
 
+
 def index(request):
-    return HttpResponse("Hello, world. index.")
+    title = "Task Manager Home"
+    return render(request, "index.html", {"title": title})
+
+
+def task_list(request):
+    tasks = Task.objects.all()
+    print(tasks)
+    return render(request, "task_list.html", {"tasks": tasks})
+
+
+def task_detail(request, task):
+    task_detail = get_object_or_404(Task, id=task)
+    return render(request, "task_detail.html", {"task": task_detail})
+
 
 def about(request):
-    return HttpResponse("Hello, world. You're at the about index.")
-    
+    return render(request, "about.html")
 
-def task_list(request,task):
-    return HttpResponse("Hello, world. You're at the task %s manager index."% task)
-    #return render(request, 'base.html')
+def ceate_task(request):
+    return render(request, "create_task.html")
 
-def task_detail(request,task):
-    task_detail= get_object_or_404(Task, id=task)
-
-
-    return HttpResponse('task %s' %task_detail.title)
